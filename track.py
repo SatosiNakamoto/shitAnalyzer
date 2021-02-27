8076ee83f6056871a87984a17b509dfe4eb86844
SatosiNakamoto <kotovsshikov31415926@gmail.com>
2021-02-27 22:12:42 +0300

    update

--- track.py
+++ track.py
@@ -6,9 +6,6 @@ from scipy.ndimage.filters import gaussian_filter, median_filter
 import math
 from skimage.filters import median, gaussian, threshold_otsu, sobel
 from skimage.morphology import binary_erosion
-upperBorder = 5700 #5700
-bottomBorder = 1000
-
 from skimage.filters import gaussian
 from skimage.segmentation import active_contour
 from skimage.color import rgb2gray
@@ -23,29 +20,60 @@ from PIL import Image
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
+upperBorder = 5700 #5700
+bottomBorder = 1000
+signTrue = lambda a: (a>0) - (a<0)
+
+def sign(a):
+	if a > 0:
+		return 1
+	elif a < 0:
+		return -1
+	else:
+		return 0
 
 class MapFrameType():
 	def __init__(self, date, pathImg = None, pathArr = None):
 		self.date = date
 		self.pathImg = pathImg
 		self.pathArr = pathArr
-		self.img = None
+		self.frame = None
 		self.map = None
-		
 		for i in os.listdir(self.pathImg):
 			if i.split(".")[0] == self.date:
-				self.img = Frame(cv.imread(self.pathImg+i))
+				self.frame = Frame(cv.imread(self.pathImg+i))
 		for i in os.listdir(self.pathArr):
 			if i.split(".")[0] == self.date:
 				self.map = Map(np.loadtxt(self.pathArr+i,skiprows = 0))
 	
 	def prepareFeatures(self):
-		self.map.setFeaturePoints(self.img.getFeaturesPoints())
+		self.map.setFeaturePoints(self.frame.getFeaturesPoints())
 
+	def gradientmap(self):
+		gr = np.gradient(self.map.map)
+		plt.imshow(gr)
+		plt.show()
+
+	def imageSegmentation(self):
+		spatialRadius = 35;
+		colorRadius = 60;
+		pyramidLevels = 3;
+		imageSegment = None
+		imageSegment = cv.pyrMeanShiftFiltering(self.frame.img, imageSegment, spatialRadius, colorRadius, pyramidLevels);
+		#cv.imshow("segmented",imageSegment)
+		#cv.waitKey(0)
+		return imageSegment
+		
 class Map():
 	def __init__(self, map):
 		self.map = map
-		
+		self.upperBorder = 5700 #5700
+		self.bottomBorder = 1000
+		self.processMap()
+
+	def processMap(self):
+		self.map[self.map <= self.bottomBorder] = 0
+		self.map[self.map >= self.upperBorder] = 0
 	#works strange.................
 	def setFeaturePoints(self,points):
 		self.map[300, 300] = np.nan
@@ -57,6 +85,23 @@ class Map():
 			print(str(int(p[0])) + " " + str(int(p[1])))
 			self.map[int(p[0]),int(p[1])] = np.nan
 		print("######################")
+
+	#0 for left
+	#1 for right
+	#works only when its first frame of truck
+	#use time to split them and disaideWhichHalfIsCloserToCar function
+	def disaideWhichHalfIsCloserToCar(self):
+		halfOfColumns = int(self.map.shape[1]/2)
+		window = self.map[0:self.map.shape[1], 0:halfOfColumns]
+		leftHalf = window.sum()
+		rightHalf = self.map.sum() - leftHalf
+		print("right half - " + str(rightHalf))
+		print("left half  - " + str(leftHalf))
+		
+		if leftHalf > rightHalf:
+			return 0
+		else:
+			return 1
 
 	def showMap(self):
 		plt.imshow(self.map)
@@ -107,30 +152,14 @@ class mapProcessor():
 			img = np.loadtxt(self.path+i,skiprows = 0)
 
 			imgs.append(Map(img))
-		return imgs		
+		return imgs
+
 	def processNMaps(self,n):
 		for map in self.getMaps(n):
 			map.map[map.map <= self.bottomBorder] = 0
 			map.map[map.map >= self.upperBorder] = 0
 			self.processedMaps.append(map)
 	
-	#0 for left
-	#1 for right
-	#works only when its first frame of truck
-	def disaideWhichHalfIsCloserToCar(self):
-		firstMap = self.processedMaps[0].map
-
-		halfOfColumns = int(firstMap.shape[1]/2)
-		sumLeftHalf=firstMap.sum(axis=0)
-		sumRightHalf=firstMap.sum(axis=0)
-		leftHalf = sumLeftHalf[halfOfColumns]
-		rightHalf = firstMap.sum() - leftHalf
-		if leftHalf > rightHalf:
-			return 0
-		else:
-			return 1
-
-	#use time to split them and disaideWhichHalfIsCloserToCar function
 	def splitCarsByItsMaps(self):
 		return None
 
@@ -139,17 +168,22 @@ class Matcher():
 	def __init__(self):
  		self.bf = cv.BFMatcher(cv.NORM_HAMMING)
  		self.last = None
- 		self.speed = 0
+ 		self.delta = 0
  		self.perfectMatches = []
  		self.kp1 = []
  		self.kp2 = []
  		self.img1 = None
  		self.img2 = None
+ 		self.map1 = None
+ 		self.map2 = None
+ 		self.frame1 = None
+ 		self.frame2 = None
+ 		self.mapShif = None
+ 		#0 - from left; 1 - from right
+ 		self.sideWhereMoveingFrom = -1
+ 		self.contourCoor = None
 
 	def drawMatches(self):
-		#if self.img1 == None or self.img2 == None or self.kp1 == [] or self.kp2 == [] or self.perfectMatches == []:
-		#	print("cant draw compare two frames first")
-		#	return
 		rows1 = self.img1.shape[0]
 		cols1 = self.img1.shape[1]
 		rows2 = self.img2.shape[0]
@@ -172,17 +206,21 @@ class Matcher():
 			cv.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
 			cv.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255,0,0), 1)
 		print("-----------------")
-		cv.line(out, (640*2 - int(self.speed), 0), (640*2 - int(self.speed), 480), (0, 255, 0), thickness=2)
+		cv.line(out, (640 + int(self.delta), 0), (640 + int(self.delta), 480), (0, 255, 0), thickness=2)
 		cv.imshow('Matched Features', out)
 		#cv.destroyWindow('Matched Features')
 		
 		return out
 
 	def compareTwoFrame(self,mapFrame1, mapFrame2):
-		features1 = mapFrame1.img.getFeaturesPoints()
-		features2 = mapFrame2.img.getFeaturesPoints()
-		self.img1 = cv.cvtColor(mapFrame1.img.img, cv.COLOR_BGR2GRAY)
-		self.img2 = cv.cvtColor(mapFrame2.img.img, cv.COLOR_BGR2GRAY)
+		features1 = mapFrame1.frame.getFeaturesPoints()
+		features2 = mapFrame2.frame.getFeaturesPoints()
+		self.img1 = cv.cvtColor(mapFrame1.frame.img, cv.COLOR_BGR2GRAY)
+		self.img2 = cv.cvtColor(mapFrame2.frame.img, cv.COLOR_BGR2GRAY)
+		self.map1 = mapFrame1.map
+		self.frame1 = mapFrame1.frame
+		self.map2 = mapFrame2.map
+		self.frame2 = mapFrame2.frame
 		matches = self.bf.knnMatch(features1[1],features2[1], k=2)
 		retdescs = []
 		retkeypoints = []
@@ -195,6 +233,7 @@ class Matcher():
 		self.kp2 = features2[0]
 		sumSpeed = 0
 		countOfChangedPoints = 0
+		directionSum = 0
 		for mat in retdescs:
 			img1_idx = mat.queryIdx
 			img2_idx = mat.trainIdx
@@ -209,71 +248,63 @@ class Matcher():
 			pathVectorLen = math.sqrt( (x2 - x1) * (x2-x1) + (y2-y1)*(y2-y1))
 			sumSpeed = sumSpeed + pathVectorLen
 			countOfChangedPoints = countOfChangedPoints + 1
+			directionSum = directionSum + sign(x2-x1)
+		self.delta = int(sumSpeed/countOfChangedPoints) * sign(directionSum)
+		print("delta is" + str(self.delta))
 		return self.perfectMatches
+	
+	def findCountoursOftruck(self):
+		diff = cv.absdiff(self.img1, self.img2)
+		blur = cv.GaussianBlur(diff, (5, 5), 0)
+		_, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
+		dilated = cv.dilate(thresh, None, iterations=3)
+		сontours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		print("cont len is" + str(len(сontours)))
+		good_cont = []
 
+		#ADD SORTINQ AND FILTERING PLS
+		for contour in сontours:
+			(x, y, w, h) = cv.boundingRect(contour)
+			print(cv.contourArea(contour))
+			if  cv.contourArea(contour) < 20000 or cv.contourArea(contour) > 100000:  
+				continue
+			good_cont.append(contour)
+			cv.rectangle(self.img2, (x, y), (x + w, y + h), (0, 255, 0), 2)  
+			self.contourCoor = [x, y, x + w, y + h]
+			#cv.drawContours(self.img2, good_cont, -1, (0, 255, 0), 2)
+		#cv.imshow("contours", self.img2)
+		#cv.waitKey(0)
+	
+	def findFirstMapMask(self):
+		self.contourCoor = 
+
+	def calcMapShift(self):
+		if self.delta > 0:
+			self.sideWhereMoveingFrom = 0
+		elif self.delta < 0:
+			self.sideWhereMoveingFrom = 1
+		else:
+			self.sideWhereMoveingFrom = -1
+
+		print("side is" + str(self.sideWhereMoveingFrom))
+		print("delta is " +  str(self.delta))
+		if self.sideWhereMoveingFrom == 1:
+			self.mapShif = Map(self.map2.map[ 0:self.map2.map.shape[0], self.map2.map.shape[1] + self.delta:self.map2.map.shape[1] ])
+		elif self.sideWhereMoveingFrom	 == 0:
+			self.mapShif = Map(self.map1.map[0:self.map1.map.shape[0], 0:self.delta])
 
 #mp = mapProcessor("img/")
-mf1 = MapFrameType("22-02-2021-00-06-42-110409", "img/image/", "img/array10/")
-mf2 = MapFrameType("22-02-2021-00-06-42-259410", "img/image/", "img/array10/")
-
+mf1 = MapFrameType("22-02-2021-00-06-42-259410", "img/image/", "img/array10/")
+mf2 = MapFrameType("22-02-2021-00-06-42-389983", "img/image/", "img/array10/")
 matcher = Matcher()
 matchesList = matcher.compareTwoFrame(mf1,mf2)
-resPoints = [matcher.kp2[i.queryIdx].pt for i in matcher.perfectMatches]
-mapp = Map(np.loadtxt("img/array10/22-02-2021-00-06-42-259410.txt"))
-#mapp.setFeaturePoints(resPoints)
-
-mf3 = MapFrameType("22-02-2021-00-06-42-259410", "img/image/", "img/array10/")
-#mf3.img.drawFeaturesPoints(resPoints)
-mapp.showMap()
-matcher.drawMatches()
-
-plt.show()
 cv.waitKey(0)
 
+matcher.findCountoursOftruck()
+matcher.drawMatches()
+matcher.calcMapShift()
+matcher.mapShif.showMap()
+print(matcher.contourCoor)
+plt.show()
+cv.waitKey(0)
 print("lol")
-'''
-
-import cv2  # импорт модуля cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-
-k = 0
-frame2 = 0
-for i in os.listdir("D:/svt/image12/"):
-    frame1 = cv2.imread("D:/svt/image12/" + i)
-    if k == 0:
-        frame2 = frame1
-        k = 1
-        continue
-    diff = cv2.absdiff(frame1, frame2)  # нахождение разницы двух кадров, которая проявляется лишь при изменении одного из них, т.е. с этого момента наша программа реагирует на любое движение.
-    #cv2.imshow('1', diff)
-
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)  # перевод кадров в черно-белую градацию
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)  # фильтрация лишних контуров
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)  # метод для выделения кромки объекта белым цветом
-    dilated = cv2.dilate(thresh, None, iterations=3)  # данный метод противоположен методу erosion(), т.е. эрозии объекта, и расширяет выделенную на предыдущем этапе область
-    сontours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # нахождение массива контурных точек
-    
-    for contour in сontours:
-        (x, y, w, h) = cv2.boundingRect(contour)  # преобразование массива из предыдущего этапа в кортеж из четырех координат
-            # метод contourArea() по заданным contour точкам, здесь кортежу, вычисляет площадь зафиксированного объекта в каждый момент времени, это можно проверить
-        print(cv2.contourArea(contour))
-        if cv2.contourArea(contour) < 700:  # условие при котором площадь выделенного объекта меньше 700 px
-            continue
-        cv2.rectangle(diff, (x, y), (x + w, y + h), (0, 255, 0), 2)  # получение прямоугольника из точек кортежа
-        #cv2.putText(frame1, "Status: {}".format("Dvigenie"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)  # вставляем текст
-
-    plt.subplot(4, 3, k)
-    plt.imshow(frame1, interpolation='nearest')
-    plt.subplot(4, 3, k + 1)
-    plt.imshow(diff, interpolation='nearest')
-    plt.subplot(4, 3, k + 2)
-    plt.imshow(frame2, interpolation='nearest')
-    k += 3
-    frame2 = frame1
-    if k > 10:
-        k = 0
-        plt.show()
-
-'''
